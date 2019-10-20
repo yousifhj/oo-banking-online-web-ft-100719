4553aa91b7fcbd8695f88d5366f4e8039c137d6b
Huda Yousif <hjdoski@gmail.com>
2019-10-20 23:57:38 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,24 @@
 class BankAccount
+  
+  attr_reader :name
+  attr_accessor :balance, :status
+
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end 
+  
+  def deposit(money)
+    @balance += money
+  end 
+  
+  def display_balance 
+    return "Your balance is $#{@balance}."
+  end
+  
+  def valid?
+    open ? 
+  end 
 
 end
