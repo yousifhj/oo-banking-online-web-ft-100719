ac078fd24213dcf2445e83677f0f599b5dd30793
Huda Yousif <hjdoski@gmail.com>
2019-10-21 00:34:39 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,12 +13,19 @@ class Transfer
   end
 
   def execute_transaction
-    if @sender.balance > @amount 
+    if @sender.balance < @amount
       @status = "rejected"
-      return "Transcation rejected. Please check your account balance."
-     elsif @status == "complete"
+      return "Transaction rejected. Please check your account balance."
+
+    elsif @status == "complete"
+      puts "Transaction was already excuted"
+    else
+      @sender.deposit( @amount * -1 ) 
+      @receiver.deposit( @amount )
+      @status = "complete"
     end
   end
+
 
 
 end
