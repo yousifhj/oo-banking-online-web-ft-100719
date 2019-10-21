e32c9d0ba0ed83d02b51e87abdbbda791c2316e8
Huda Yousif <hjdoski@gmail.com>
2019-10-21 00:21:50 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -20,7 +20,7 @@ class Transfer
       @status = "complete"
    else
       @status = "rejected"
-      return "Transaction rejected. Please check your account balance."
+     "Transaction rejected. Please check your account balance."
     end
   end
   
