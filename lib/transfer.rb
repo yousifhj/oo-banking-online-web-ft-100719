574e81bc79abd673eb5ac410a3b8a4c3da4207bf
Huda Yousif <hjdoski@gmail.com>
2019-10-21 00:11:09 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,6 +1,6 @@
 class Transfer
   
-  attr_accessor :sender, :receiver, :amount
+  attr_accessor :sender, :receiver, :amount, :status
   
   def initialize(sender, receiver, amount)
     @sender = sender
