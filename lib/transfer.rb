23a8e077c052473f3565a7f418a7c1ebac95d8b0
Huda Yousif <hjdoski@gmail.com>
2019-10-21 00:35:21 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -26,6 +26,13 @@ class Transfer
     end
   end
 
+  def reverse_transfer
+    if @status == "complete"
+      @sender.deposit( @amount ) 
+      @receiver.deposit( @amount * -1)
+      @status = "reversed"
+    end
+  end
 
 
 end
