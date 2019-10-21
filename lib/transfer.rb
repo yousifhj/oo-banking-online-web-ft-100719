4d161c219ccf6fc030a414ab7a281313bc408ccf
Huda Yousif <hjdoski@gmail.com>
2019-10-21 00:17:52 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,7 +17,7 @@ class Transfer
     if @sender.balance > @amount && @status == "pending"
       @sender.balance -= @amount
       @receiver.balance += @amount
-      @status "complete"
+      @status = "complete"
     else 
       @status = "rejected"
       
