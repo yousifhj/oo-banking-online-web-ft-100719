361936e0e7952518fbbfa9a7dd2647543e36662f
Huda Yousif <hjdoski@gmail.com>
2019-10-21 00:28:43 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,23 +13,22 @@
     @receiver.valid? && @sender.valid? ? true : false 
   end 
   
-  def execute_transaction
+ def execute_transaction
     if @sender.balance > @amount && @status == "pending"
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
     else
-      @status = "reject"
-      return "Complete"
+      @status = "rejected"
+      return "Transaction rejected. Please check your account balance."
     end
   end
-  
-    def reverse_transfer
+
+  def reverse_transfer
     if @status == "complete"
       @sender.balance += @amount
       @receiver.balance -= @amount
       @status = "reversed"
     end
   end
-
-end 
+end
